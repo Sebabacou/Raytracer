4879d9d9e3390100a83a7bd1b7f2f4ff9b9bd876
Sebabacou <sebastien.bertilsouchet@protonmail.com>
2024-04-25 16:31:40 +0200

    feat: src folder

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** Raytracer
+** File description:
+** main.cpp
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
