b2666ac240f1e633ea129b157fa0f933ae636211
Diabolo <alexis.boitel@epitech.eu>
2024-04-29 13:39:20 +0200

    test(): dlloader

--- src/DLLoader/DLLoader.hpp
+++ src/DLLoader/DLLoader.hpp
@@ -11,66 +11,50 @@
 #include <string>
 #include <stdexcept>
 #include <dlfcn.h>
+#include <memory>
+#include <DLSym.hpp>
 
-
-    template <typename T>
     class DLLoader {
         public:
             /**
              * @brief Construct a new DLLoader object
              *
+             * @param path path to the library
+             * @param entryPoint name of the function to load
              */
-            DLLoader() = default;
+            DLLoader(std::string path, std::string entryPoint) {
+                try {
+                    _handle = dlopen(path.c_str(), RTLD_LAZY);
+                    if (!_handle) {
+                        throw std::runtime_error("Cannot open library: " + std::string(dlerror()));
+                    }
+                } catch (const std::exception &e) {
+                    throw std::runtime_error("Cannot open library: " + std::string(dlerror()));
+                }
+            }
 
             /**
              * @brief Destroy the DLLoader object
              *
              */
             ~DLLoader() {
-                if (_handle)
+                if (_handle) {
                     dlclose(_handle);
+                }
             }
 
-            /**
-             * @brief Get the Instance object
-             *
-             * Use this function to get an instance of the class
-             * Throw a runtime_error if the library cannot be opened or if the symbol cannot be loaded
-             *
-             * @param path path to the library
-             * @param entryPoint name of the function to load
-             * @return T*
-             */
-            T *getInstance(const std::string &path, const std::string &entryPoint = "entryPoint")
-            {
-                _handle = dlopen(path.c_str(), RTLD_LAZY);
-                if (!_handle) {
-                    throw std::runtime_error("Cannot open library: " + std::string(dlerror()));
-                }
-                void *(*create)() = (void *(*)())dlsym(_handle, entryPoint.c_str());
-                if (!create) {
+            template<typename T>
+            T getFunction(const std::string &entryPoint) {
+                try {
+                    DLSym<T> sym(_handle);
+                    return sym.getFunction(entryPoint);
+                } catch (const std::exception &e) {
                     throw std::runtime_error("Cannot load symbol: " + std::string(dlerror()));
-                }
-                T *instance = static_cast<T *>(create());
-
-                return instance;
-                // return nullptr;
-            }
-
-            /**
-             * @brief Destroy the Instance object
-             *
-             * @param instance
-             */
-            void destroyInstance(T *instance)
-            {
-                if (instance) {
-                    delete instance;
                 }
             }
 
         private:
-            void *_handle = nullptr;
+            void std::shared_ptr<void> _handle;
     };
 
 
