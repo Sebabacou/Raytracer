081abb4e4816e1c61034d11c9f2639564e8f9aeb
3w3n <ewen.emeraud@epitech.eu>
2024-04-30 10:43:44 +0200

    fix(DLLOADER):fix function loader and stop using DLSym

--- src/DLLoader/DLLoader.hpp
+++ src/DLLoader/DLLoader.hpp
@@ -12,50 +12,41 @@
 #include <stdexcept>
 #include <dlfcn.h>
 #include <memory>
-#include <DLSym.hpp>
 
-    class DLLoader {
-        public:
-            /**
-             * @brief Construct a new DLLoader object
-             *
-             * @param path path to the library
-             * @param entryPoint name of the function to load
-             */
-            DLLoader(std::string path, std::string entryPoint) {
-                try {
-                    _handle = dlopen(path.c_str(), RTLD_LAZY);
-                    if (!_handle) {
-                        throw std::runtime_error("Cannot open library: " + std::string(dlerror()));
-                    }
-                } catch (const std::exception &e) {
-                    throw std::runtime_error("Cannot open library: " + std::string(dlerror()));
-                }
+class DLLoader {
+public:
+    DLLoader(std::string path) {
+        try {
+            _handle = dlopen(path.c_str(), RTLD_LAZY);
+            if (!_handle) {
+                throw std::runtime_error("Cannot open library: " + std::string(dlerror()));
             }
+        } catch (const std::exception &e) {
+            throw std::runtime_error("Cannot open library: " + std::string(dlerror()));
+        }
+    }
+    ~DLLoader() {
+        if (_handle)
+            dlclose(_handle);
+    }
 
-            /**
-             * @brief Destroy the DLLoader object
-             *
-             */
-            ~DLLoader() {
-                if (_handle) {
-                    dlclose(_handle);
-                }
-            }
+    template<typename T>
+    T getFunction(const std::string &entryPoint) {
+        void *(*instance)() = (void *(*)())dlsym(_handle, entryPoint.c_str());
+        if (!instance) {
+            throw std::runtime_error("Cannot load symbol: " + std::string(dlerror()));
+        }
+        return static_cast<T>(instance);
+    }
 
-            template<typename T>
-            T getFunction(const std::string &entryPoint) {
-                try {
-                    DLSym<T> sym(_handle);
-                    return sym.getFunction(entryPoint);
-                } catch (const std::exception &e) {
-                    throw std::runtime_error("Cannot load symbol: " + std::string(dlerror()));
-                }
-            }
+    template<typename T>
+    void destroyInstance(T instance) {
+        delete instance;
+    }
 
-        private:
-            void std::shared_ptr<void> _handle;
-    };
+private:
+    void *_handle;
+};
 
 
 #endif /* !DLLOADER_HPP_ */
